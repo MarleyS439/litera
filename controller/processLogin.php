d871f7787c7dbbcb2b93b215ad60750e35c3c578
MarleyS439 <marleysantos439@gmail.com>
2024-04-26 17:21:40 -0300

    Início das modificações de telas de cadastro de usuário e crinaças

--- controller/processLogin.php
+++ controller/processLogin.php
@@ -1,36 +1,51 @@
 <?php
 // caminhos para os arquivos dao e model
 require_once("../dao/usuarioDao.php");
-require_once("../dao/dadosJogoUsuarioDao.php");
-
+require_once(__DIR__."../../models/usuario.php");
+// variavel que vai mandar os dados para o model
+$usuario = new Usuario();
+// Verifica se o cadastro pode ser realizado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $data = json_decode(file_get_contents('php://input'), true);
-    if ($data !== null) {
-        $id = $data['id'];
-        $idFase = $data['idFase'];
-        $pontos = $data['pontos'];
-        $acertos = $data['acertos'];
-        $erros = $data['erros']; // Corrigido para erros
+    // Faz a verificação se todos os campos foram preenchidos corretamente
+    if (!empty($_POST['login_user']) && !empty($_POST['passwd_user'])) {
+        // Todos os campos estão preenchidos corretamente
+        // Esse array é apenas para o var dump ***
+        $dados = array(
+            'login' => $_POST['login_user'],
+            'Senha' => $_POST['passwd_user'],
+        );
+        // try catch para conexão com o banco
         try {
-            // Consulta dos dados do jogo do usuário
-            $dadosJogo = DadosJogoUsuarioDao::selectById($id, $idFase);
-            if ($dadosJogo) {
-                // Atualiza os dados do jogo do usuário no banco de dados
-                $resultado = DadosJogoUsuarioDao::update($id, $idFase, $pontos, $acertos, $erros);
-                if ($resultado) {
-                    echo "Insert feito com sucesso!";
-                } else {
-                    echo "Erro ao inserir os valores!";
-                }
-            } else {
-                echo "Dados do jogo do usuário não encontrados";
-            }
+            $conexao = Conexao::conectar();
         } catch (PDOException $e) {
-            echo "Erro na conexão com o banco de dados: " . $e->getMessage();
+            die("Erro na conexão com o banco de dados: " . $e->getMessage());
         }
-    } else {
-        echo "Dados inválidos recebidos"; // Responde ao cliente se os dados recebidos forem inválidos
+        // inicio da verificaçao, conexao com o Dao
+        $usuarioDao = UsuarioDao::checkCredentials($_POST['login_user'], $_POST['passwd_user']);
+        // iniciando uma session caso login seja realizado
+        if ($usuarioDao) {
+            // vetor com as informaçoes do usuario
+            $authUser = [
+                'cod' => $usuarioDao['codUsuario'],
+                'nome' => $usuarioDao['nomeUsuario'],
+                'email' => $usuarioDao['emailUsuario'],
+                'senha' => $_POST['passwd_user'],
+                'pontuacao' => $usuarioDao['pontuacaoUsuario'],
+                'dinheiro' => $usuarioDao['dinheiroUsuario'],
+                'tutorial' => $usuarioDao['tutorial'],
+                'banido' => $usuarioDao['banido'],
+            ];
+            // Iniciar a sessão apenas se as credenciais estiverem corretas
+            session_start();
+            $_SESSION["authUser"] = $authUser;
+            if($authUser['tutorial'] == 0){
+                header("Location: ../views/avatar.php?status=base");
+            }else{
+                header("Location: ../views/home.php");
+            }
+        } else {
+            // caso negado, redirecionar para a página de login com uma mensagem de erro
+            header("Location: ../views/login.php?status=erro1");
+        }
     }
-} else {
-    echo "Método de requisição inválido"; // Responde ao cliente se o método de requisição for inválido
 }
--- views/register.php
+++ views/register.php
@@ -8,249 +8,56 @@
     <meta name="keywords" content="">
     <meta name="author" content="Illumi">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Litera</title>
-    <link rel="shortcut icon" href="../assets/images/icons/Litera Icon2.ico" type="image/x-icon">
+    <title>Litera | Cadastro</title>
+    <link rel="shortcut icon" href="../assets/images/icons/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="../assets/css/register.css">
 </head>
 
 <body>
-
-    <div class="container-page">
-        <div class="sidea">
-            <div class="lit">
-                <h2>Litera</h2>
-                <span>Alfabetizando novas gerações</span>
-                <div class="litera">
-                    <img src="../assets/images/arara 2.svg" alt="">
-                </div>
-            </div>
-
+<div class="overlay"></div>
+    <div class="container">
+        <div class="arara">
+            <img src="../assets/images/arara 2.svg" alt="">
+            <h2>Litera</h2>
         </div>
 
-        <div class="sideb">
+        <div class="container-form">
             <div class="title-form">
                 <h2>Seja bem-vindo</h2>
-                <span>Vamos criar a sua conta</span>
+                <p>Vamos criar a sua conta</p>
             </div>
 
-            <form action="../controller/processRegister.php" method="post">
+            <form action="" method="post">
                 <div class="inputs">
-                    <label for="responsavel">Nome do responsável</label>
-                    <input type="text" name="name_user" id="responsavel" placeholder="Nome completo" required>
+                    <label for="">Nome do responsável</label>
+                    <input type="text" name="" id="" placeholder="Digite o seu nome" required>
                 </div>
 
                 <div class="inputs">
-                    <label for="email">E-mail</label>
-                    <input type="email" name="email_user" id="email" placeholder="Digite seu melhor e-mail" required>
+                    <label for="">E-mail</label>
+                    <input type="email" name="" id="" placeholder="email@email.com" required>
                 </div>
 
                 <div class="inputs">
-                    <label for="senha">Senha</label>
-                    <input type="password" name="passwd_user" id="senha" placeholder="Palavra passe" required>
+                    <label for="">Senha</label>
+                    <input type="password" name="" id="" placeholder="Digite sua senha" required>
                 </div>
-
                 <div class="inputs">
-                    <label for="comfirm">Confirmar senha</label>
-                    <input type="password" name="confirm_passwd_user" id="confirm" placeholder="Confirmar palavra passe" required>
+                    <label for="">Confirmar senha</label>
+                    <input type="password" name="" id="" placeholder="Confirme sua senha" required>
                 </div>
 
-               <!-- <div class="t-inputs">
-                    <div class="inputs">
-                        <label for="idade">Nascimento</label>
-                        <input type="date" name="idade_crianca" id="idade" placeholder="">
-                    </div>
-
-                    <div class="inputs">
-                        <label for="genero">Gênero</label>
-                        <select name="genero_crianca" id="genero">
-                            <option value="">Não definido</option>
-                            <option value="Masculino">Masculino</option>
-                            <option value="Feminino">Feminino</option>
-                        </select>
-                    </div>
-                </div> -->
+                <div class="inputs btn">
+                    <button type="submit">Criar</button>
+                </div>
 
                 <div class="non">
-                    <p>Já possui cadastro?</p>
-                    <a href="login.php">Faça login</a>
-                </div>
-
-                <div class="advisor">
-                    <!-- Aqui vai as informações de autenticação de login -->
-                </div>
-
-                <div class="send">
-                    <button type="submit">
-                        <span>Próximo</span>
-                        <img src="../assets/images/icons/next-svgrepo-com.svg" alt="">
-                    </button>
+                    <p>Já possui conta?</p>
+                    <a href="login.php">Fazer login</a>
                 </div>
             </form>
         </div>
     </div>
-    <!-- Do not have more mobile-view div. Now is a unique div -->
-    <!-- <div class="desktop-view">
-        <div class="container-desktop">
-            <div class="content">
-                <div class="title">
-                    <h1>Litera</h1>
-                    <p>Alfabetizando novas gerações</p>
-                </div>
-
-                <div class="arara-imagem">
-                    <img src="../assets/images/arara 2.svg" alt="">
-                </div>
-            </div>
-
-            <div class="formu">
-                <form action="../controller/processRegister.php" method="post">
-                    <h2>Seja bem-vindo</h2>
-                    <div class="create-acc">
-                        <p>Vamos criar a sua conta</p>
-                    </div>
-                    <div class="item">
-                        <label for="nameUser">Seu nome</label>
-                        <input type="text" name="name_user" id="nameUser" placeholder="" required>
-                    </div>
-
-                    <div class="item">
-                        <label for="nameUser">Seu nome de usuário</label>
-                        <input type="text" name="login_user" id="nameUser" placeholder="" required>
-                    </div>
-
-                    <div class="item">
-                        <label for="nameUser">E-mail</label>
-                        <input type="text" name="email_user" id="nameUser" placeholder="" required>
-                    </div>
-
-                    <div class="item">
-                        <label for="passwUser">Senha</label>
-                        <input type="password" name="passwd_user" id="passwUser" required>
-                    </div>
-
-                    <div class="item">
-                        <label for="passwUser">Confrimar senha</label>
-                        <input type="password" name="confirm_passwd_user" id="passwUser" required>
-                    </div>
-
-                    <div class="item">
-                        <button type="submit">Continuar</button>
-                    </div>
-
-                    <div class="register">
-                        <p>Já tem cadastro? <a href="../views/login.php">Faça login</a></p>
-                    </div>
-
-                    <div class="return">
-                        <a href="./../index.php">Voltar</a>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <!-- View de desktop -->
-    <!-- <div class="desktop-screen">
-travou kkkkk
-        <div class="container-desktop">
-
-            <div class="side1">
-                <div class="title-desktop">
-                    <h1>Litera</h1>
-                    <p>Alfabetizando novas gerações</p>
-                </div>
-                <div class="arara-desktop">
-                    <div class="arara-img">
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="side2">
-                <div class="right-i">
-                    <h2>Seja bem-vindo</h2>
-                    <div class="lets">
-                        <p>Vamos criar a sua conta</p>
-                    </div>
-                    <div class="btns">
-                        <form action="../controller/processRegister.php" method="POST">
-
-                            <div class="item-form">
-                                <label for="nameUser">Seu nome</label>
-                                <input type="text" name="name_user" id="nameUser" required>
-                            </div>
-
-                            <div class="item-form">
-                                <label for="loginUser">E-mail</label>
-                                <input type="email" name="email_user" id="loginUser" required placeholder="email@email.com">
-                            </div>
-
-                            <div class="item-form">
-                                <label for="passwd">Senha</label>
-                                <input type="password" name="passwd_user" id="passwd" required>
-                            </div>
-
-                            <div class="item-form">
-                                <label for="confirmation">Confirmar senha</label>
-                                <input type="password" name="confirm_passwd_user" id="confirmation" required>
-                            </div>
-
-                            <div class="item-form">
-                                <button type="submit">Continuar</button>
-                            </div>
-
-                            <div class="item-form">
-                                <div class="link">
-                                    <a href="../index.php" id="returns">Voltar</a>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div> -->
-    <!-- View de mobile e tablet -->
-
-
-    <!--<div class="mobile-screen">
-        <div class="logo">
-            <img src="../assets/images/LOGOTIPO (cópia).png" alt="">
-        </div>
-
-        <div class="buttons">
-            <form action="../controller/processRegister.php" method="POST">
-                <div class="item-form-m">
-                    <label for="nameUser">Seu nome</label>
-                    <input type="text" name="name_user" id="nameUser" required>
-                </div>
-
-                <div class="item-form-m">
-                    <label for="loginUser">E-mail</label>
-                    <input type="text" name="email_user" id="loginUser" required placeholder="email@email.com">
-                </div>
-
-                <div class="item-form-m">
-                    <label for="passwd">Senha</label>
-                    <input type="password" name="passwd_user" id="passwd">
-                </div>
-
-                <div class="item-form-m">
-                    <label for="confirmation">Confirmar senha</label>
-                    <input type="password" name="confirm_passwd_user" id="confirmation" required>
-                </div>
-
-                <div class="item-form-m">
-                    <button type="submit">Continuar</button>
-                </div>
-
-                <div class="item-form-m">
-                    <a href="./index.php" id="return">Voltar</a>
-                </div>
-            </form>
-        </div>
-    </div> -->
-
 </body>
 
 </html>
