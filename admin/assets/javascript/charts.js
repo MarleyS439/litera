514767283514e45b7bd4f9675cceb18ba548292b
Tyran15 <mh15122006@gmail.com>
2024-05-26 19:17:20 -0300

    Estilização dos card e gráficos na home de admin

--- admin/assets/javascript/charts.js
+++ admin/assets/javascript/charts.js
@@ -1,82 +1,124 @@
-let mesAtual = document.getElementById("mes").value;
+const lineChart = document.getElementById('lineChart').getContext('2d');
+const doughnutChart1 = document.getElementById('doughnutChart1').getContext('2d');
+const doughnutChart2 = document.getElementById('doughnutChart2').getContext('2d');
+const doughnutChart3 = document.getElementById('doughnutChart3').getContext('2d');
 
-var dados = [];
+// Criar gradiente
+const gradient = lineChart.createLinearGradient(0, 0, 0, 400);
+gradient.addColorStop(0, 'rgba(110, 200, 239, 1)'); // Cor inicial
+gradient.addColorStop(1, 'rgba(255, 255, 255, 0.3)');   // Cor final
 
-// Loop para iterar sobre os dados do PHP e construir os dados para o gráfico
-for (var mes = 1; mes <= mesAtual; mes++) {
-  dados.push(quantidade_por_mes[mes] || 0); // Adiciona o dado do mês, se não estiver definido, define como 0
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const ctx = document.getElementById("myChart").getContext("2d");
-
-  new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: [
-        "Jan",
-        "Fev",
-        "Mar",
-        "Abr",
-        "Mai",
-        "Jun",
-        "Jul",
-        "Ago",
-        "Set",
-        "Out",
-        "Nov",
-        "Dez",
-      ],
-      datasets: [
-        {
-          label: "Usuários registrados",
-          data: dados,
-          borderColor: "#FFFFFF",
-          backgroundColor: "#FFFFFF",
-          borderWidth: 5,
+new Chart(lineChart, {
+  type: 'line',
+  data: {
+    labels: ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sabado'],
+    datasets: [{
+      data: [12, 19, 3, 5, 2, 3, 7],
+      tension: 0.2,
+      borderWidth: 1,
+      borderColor: 'rgb(45, 156, 219)',
+      backgroundColor: gradient,
+      borderWidth: 3,
+      fill: true,
+    }]
+  },
+  options: {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Acessos',
+        align: 'start',
+        font: {
+          size: 24 // Tamanho da fonte do título
         },
-      ],
-    },
-    options: {
-      layout: {
         padding: {
-          left: 5,
-          right: 40,
-          top: 20,
-          bottom: 20,
-        },
-        responsive: true,
-        maintainAspectRatio: false,
+          top: 10 // Espaçamento superior do título
+        }
       },
-      scales: {
-        x: {
-          ticks: {
-            color: "#FFFFFF", // Cor da fonte do eixo X
-            fontWeight: "bold",
-          },
-          grid: {
-            color: "#4180AB", // Cor das linhas de fundo do eixo X
-          },
+      subtitle: {
+        display: true,
+        text: 'Acessos de usuários nesta semana',
+        align: 'start',
+        font: {
+          size: 16 // Tamanho da fonte do subtítulo
         },
-        y: {
-          ticks: {
-            color: "#FFFFFF", // Cor da fonte do eixo Y
-            fontWeight: "bold",
-          },
-          beginAtZero: true,
-          grid: {
-            color: "#4180AB", // Cor das linhas de fundo do eixo Y
-          },
-        },
+        padding: {
+          top: 5, // Espaçamento superior do subtítulo
+          bottom: 50,
+        }
       },
-      plugins: {
-        legend: {
-          labels: {
-            color: "#FFFFFF", // mudar a cor da fonte da legenda
-            fontWeight: "bold",
-          },
-        },
-      },
+      legend: {
+        display: false // Oculta a legenda
+      }
     },
-  });
+    scales: {
+      x: {
+        grid: {
+          display: false // Esconde as linhas de grade do eixo X
+        }
+      },
+      y: {
+        display: false, // Esconde o eixo Y
+        ticks: {
+          display: false // Esconde os ticks do eixo Y
+        },
+        grid: {
+          display: false // Esconde as linhas de grade do eixo Y
+        }
+      }
+    }
+  },
 });
+
+
+new Chart(doughnutChart1, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      label: '# of Votes',
+      data: [81, 19],
+      backgroundColor: [
+        'red',
+        'blue',
+      ]
+    }]
+  },
+  options: {
+    // Suas opções de gráfico de pizza aqui
+  }
+});
+
+new Chart(doughnutChart2, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      label: '# of Votes',
+      data: [22, 78],
+      backgroundColor: [
+        'red',
+        'blue',
+      ]
+    }]
+  },
+  options: {
+    // Suas opções de gráfico de pizza aqui
+  }
+});
+
+new Chart(doughnutChart3, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      label: '# of Votes',
+      data: [62, 38],
+      backgroundColor: [
+        'red',
+        'blue',
+      ]
+    }]
+  },
+  options: {
+    // Suas opções de gráfico de pizza aqui
+  }
+});
+
