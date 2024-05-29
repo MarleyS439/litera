79e5ccff6a81e78954399b537bb3e87928e79a69
MarleyS439 <marleysantos439@gmail.com>
2024-05-28 23:52:39 -0300

    Criação de pasta de jogo 3
    Arquivos principais criados. No aguardo de assets para inserção em jogo

---
+++ views/jogo/03/fazenda-dos-bichos.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-br" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A Fazenda dos Bichos</title>
+</head>
+<body>
+    <div class="container-game">
+        
+    </div>
+</body>
+</html>
