19ac7a3a09c108e25f88e54172881a327203c617
Tyran15 <mh15122006@gmail.com>
2024-05-31 16:11:05 -0300

    Correção na conexão com banco de dados

--- config/conexao.php
+++ config/conexao.php
@@ -5,10 +5,10 @@ class Conexao
     {
         try {
             //informações do banco de dados
-            $servidor = "mysql-phpmyadmin";
+            $servidor = "localhost";
             $banco = "dblitera";
             $usuario = "root";
-            $senha = "12345";
+            $senha = "";
 
             //$conexao = new PDO("TIPO_BANCO:host=SERVIDOR;dbname=NOME_BANCO", "USUARIO", "SENHA"); 
             $conexao = new PDO("mysql:host=$servidor;dbname=$banco;charset=utf8", $usuario, $senha);
