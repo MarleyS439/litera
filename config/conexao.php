7231a5a2c5195bfdd3a94c3a288432201d7db532
Tyran <68672444+Tyran15@users.noreply.github.com>
2024-06-01 22:11:32 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -5,10 +5,10 @@ class Conexao
     {
         try {
             //informações do banco de dados
-            $servidor = "mysql";
+            $servidor = "localhost";
             $banco = "dblitera";
             $usuario = "root";
-            $senha = "mysql";
+            $senha = "";
 
             //$conexao = new PDO("TIPO_BANCO:host=SERVIDOR;dbname=NOME_BANCO", "USUARIO", "SENHA"); 
             $conexao = new PDO("mysql:host=$servidor;dbname=$banco;charset=utf8", $usuario, $senha);
