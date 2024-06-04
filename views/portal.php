b3ddbbc931953d387cc009a19f8f5c7e31461830
MarleyS439 <marleysantos439@gmail.com>
2024-06-04 01:17:54 -0300

    Adição do portal
    Login ou Cadastro

---
+++ views/portal.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="pt-BR" dir="ltf">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/images/litera.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="../assets/css/portal.css">
+</head>
+<body>
+    <div class="container-selection">
+        <div class="litera">
+            <img src="../assets/images/arara 2.svg" alt="Litera">
+        </div>
+
+        <div class="selections">
+            <div class="select-action">
+                <div class="title-action">
+                    <h2>Muito divertido e o melhor, é gratuito!</h2>
+                </div>
+
+                <div class="actions">
+                    <div class="option1">
+                        <a href="#">Começar agora</a>
+                    </div>
+
+                    <div class="option2">
+                        <a href="">Já possuo conta</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
