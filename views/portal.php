7c7e91cee5e889dda2ed4fae9a26951b75382b58
MarleyS439 <marleysantos439@gmail.com>
2024-06-04 23:06:33 -0300

    Inserção de estilos para o portal
    Portal de seleção - login ou cadastro - Ainda não há link direto

--- views/portal.php
+++ views/portal.php
@@ -1,35 +1,65 @@
 <!DOCTYPE html>
+<html lang="pt-BR" dir="ltr">
 
-<html lang="pt-BR" dir="ltf">
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Illumi">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="../assets/images/litera.png" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="../assets/css/portal.css">
+    <title>Litera | Login</title>
+    <link rel="stylesheet" href="../assets/css/portal.css">
+    <link rel="stylesheet" href="../assets/css/animations.css">
+    <link rel="shortcut icon" href="../assets/images/icons/favicon.ico" type="image/x-icon">
 </head>
+
 <body>
-    <div class="container-selection">
-        <div class="litera">
-            <img src="../assets/images/arara 2.svg" alt="Litera">
-        </div>
 
-        <div class="selections">
-            <div class="select-action">
-                <div class="title-action">
-                    <h2>Muito divertido e o melhor, é gratuito!</h2>
-                </div>
-
-                <div class="actions">
-                    <div class="option1">
-                        <a href="#">Começar agora</a>
-                    </div>
-
-                    <div class="option2">
-                        <a href="">Já possuo conta</a>
-                    </div>
+    <div class="container-page">
+        <div class="sidea">
+            <div class="lit">
+                <h2>Litera</h2>
+                <span>Alfabetizando novas gerações</span>
+                <div class="litera">
+                    <img src="../assets/images/arara 2.svg" alt="">
                 </div>
             </div>
+
+        </div>
+
+        <div class="sideb">
+            <div class="title-form">
+                <h2>Entrar</h2>
+            </div>
+
+            <form action="../controller/processLogin.php" method="post">
+                <div class="inputs">
+                    <label for="email">E-mail</label>
+                    <input type="email" name="login_user" id="email" placeholder="E-mail do responsável" required>
+                </div>
+
+                <div class="inputs">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="passwd_user" id="senha" placeholder="Palavra passe" required>
+                </div>
+
+                <div class="non">
+                    <p>Não possui cadastro?</p>
+                    <a href="register.php">Cadastre-se</a>
+                </div>
+
+                <div class="send">
+                    <button type="submit">
+                        <span>Continuar</span>
+                        <img src="../assets/images/icons/enter-icon.svg" alt="">
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
+
+
 </body>
+<script src="../assets/javascript/emptyField.js"></script>
+
 </html>
