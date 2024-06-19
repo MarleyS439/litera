02bbd218d262eead2a3ec955f13f80f372b4957c
MarleyS439 <marleysantos439@gmail.com>
2024-06-19 02:38:44 -0300

    Inserção de modal de consentimento de cookies

---
+++ assets/landing/modalCookies.js
@@ -0,0 +1,13 @@
+function closeModalCookies() {
+    let rejectBtn;
+    let acceptBt;
+
+    let modal = document.getElementById('cookies');
+
+    acceptBtn.document.getElementById('accept');
+    rejectBtn.document.getElementById('reject');
+
+    acceptBtn.addEventListener("click", function() {
+        modal.style.display =  "none";
+    })
+}
