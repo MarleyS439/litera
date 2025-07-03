defc2c3da27cfae3a2afe5d931cfe20a76171833
Marley de Sousa Santos <marley@localhost.localdomain>
2025-07-03 12:43:23 -0300

    Melhoria em comentários
    Melhorias em comentários do arquivo index.php
    
    Signed-off-by: Marley de Sousa Santos <marley@localhost.localdomain>

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 
-// Declaração de tipagem forte
 declare(strict_types=1);
 
-// Importa a classe Router do namespace App
 use App\Router;
 
 // Habilita mostrar erros
@@ -11,12 +9,8 @@ ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
 
-// Autoload do composer
 require_once __DIR__ . "/vendor/autoload.php";
 
-// Instância a cllasse Router
 $route = new Router();
 
-// Chama o método route() da instância da classe Router
 $route->route();
-?>
